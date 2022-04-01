68cbd27d28cc6628e30d36359529c35cb9c4af10
Ambarisha-006 <57527643+Ambarisha-006@users.noreply.github.com>
2022-04-02 03:14:10 +0530

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-export JRE_URL=https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b03/OpenJDK8U-jdk_x64_linux_hotspot_8u212b03.tar.gz
+#export JRE_URL=https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b03/OpenJDK8U-jdk_x64_linux_hotspot_8u212b03.tar.gz
+export JRE_URL=https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.9.1%2B1/OpenJDK11U-jdk_x64_linux_hotspot_11.0.9.1_1.tar.gz
 export SONARQUBE_DIST_URL="https://binaries.sonarsource.com/Distribution"
 
 export SONARQUBE_ZIP="sonarqube-${SONARQUBE_VERSION}.zip"
